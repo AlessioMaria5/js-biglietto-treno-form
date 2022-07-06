729fd994447687e6b92a00baac171f38d9e4b7c9
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-06 11:20:28 +0200

    re-factory post correzione

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const invia = document.getElementById('clientData');
 
+        // TASTO GENERA // 
+
     invia.addEventListener('click', 
-     function() {
+     function(){
 
         const animation = document.getElementById('ticket');
         animation.classList.add('animation');
@@ -16,64 +18,56 @@ const invia = document.getElementById('clientData');
         let sconto = document.getElementById('listaSconto').value; 
         let prezzoFinale;
 
-        if(sconto == 'EndPlus') {
-            
+        if(sconto == 'EndPlus') {      
             prezzoFinale = ( prezzo * 0.8 );
-
         }
 
-        if(sconto == 'Standard') {
-            
-            prezzoFinale = prezzo;
-            
+        if(sconto == 'Standard') {     
+            prezzoFinale = prezzo;     
         }
 
         if(sconto == 'Over') {
-
-            prezzoFinale = ( prezzo * 0.6);
-
-            
+            prezzoFinale = ( prezzo * 0.6);     
         }
-
-        prezzoFinale = prezzo.toFixed(2);
+            // VALORE ELEMENTI UTENTE //
 
          document.getElementById('namp').innerHTML = nome.value;
          document.getElementById('off_n').innerHTML = sconto;
          document.getElementById('carr_n').innerHTML =  Math.floor(Math.random() * 20 + 1);
          document.getElementById('cod_cp').innerHTML = Math.floor(Math.random() * 9999 + 90000 );
-         document.getElementById('tkt_p').innerHTML = prezzoFinale + '€';
-
-
-            let bigliettoGenerato = document.getElementById('tkt');
-            bigliettoGenerato.style.display = 'flex';
-
+         document.getElementById('tkt_p').innerHTML = prezzoFinale.toFixed(2) + '€';
            
-            const delay = setTimeout(myDelay, 1000);
-
+            setTimeout(myDelay, 500);
             function myDelay() {
+                let bigliettoGenerato = document.getElementById('tkt');
+                bigliettoGenerato.style.display = 'flex';
+
+                
+
+        }
+
+            setTimeout(myDelay2, 1000);
+            function myDelay2() {
 
             let successo = document.getElementById('success2');
             successo.style.display = 'block'
+        }
+    }); // fine TASTO GENERA //
+
+        // TASTO ANNULA //
+
+        const annulla = document.getElementById('delete');
+        bigliettoGenerato = document.getElementById('tkt');
             
+        annulla.addEventListener('click', 
+            function(){ 
+            bigliettoGenerato.style.display = 'none';
 
-            }
-            
-        // alert(`I valori inseriti sono: ${nome.value} ${distanza.value}Km ${sconto.value}`);
-    }
-
-);
-
-const annulla = document.getElementById('delete');
-bigliettoGenerato = document.getElementById('tkt');
-            
-annulla.addEventListener('click', 
-function(){ 
-    bigliettoGenerato.style.display = 'none';
-
-    let successo = document.getElementById('success2');
-        successo.style.display = 'none'
-
-} );
+            let successo = document.getElementById('success2');
+            successo.style.display = 'none'
+        } );
+    
+        // FINE TASTO ANULLA //
 
     
 
